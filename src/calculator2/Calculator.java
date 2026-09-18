018a8ad72a85839718178ba7caa7991cd8dc69df
myhyeoeo <dk9790@naver.com>
2026-09-18 12:38:44 +0900

    feat: calc 메서드 구현, 컬렉션 타입 필드 가짐

--- src/calculator2/Calculator.java
+++ src/calculator2/Calculator.java
@@ -1,5 +1,40 @@
 package calculator2;
 
+import java.util.ArrayList;
+
 public class Calculator {
+    private ArrayList<Integer> arrayList = new ArrayList<>();
+
+    public int calc(int a, int b, char op){
+        int result = 0;
+
+        if(a<0 || b<0){
+            System.out.println("첫 번째 숫자와 두 번째 숫자는 양의 정수여야 합니다");
+            return 0;
+        }
+
+        if(op == '+'){
+            result = a+b;
+        }
+        else if(op == '-'){
+            result = a-b;
+        }
+        else if(op == '*'){
+            result = a*b;
+        }
+        else if(op == '/'){
+            if(b==0){
+                System.out.println("분모는 0이 될 수 없습니다");
+                return 0;
+            }
+            result = a/b;
+        }
+        else{
+            System.out.println("잘못된 연산자입니다");
+        }
+        arrayList.add(result);
+        return result;
+    }
+
 
 }
