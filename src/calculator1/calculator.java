8659679971b01e6b76a02deccfe723f0a94d7925
myhyeoeo <dk9790@naver.com>
2026-09-18 09:39:31 +0900

    feat: 양의 정수(0 포함)를 입력받기

---
+++ src/calculator1/calculator.java
@@ -0,0 +1,16 @@
+package calculator1;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        a = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        b = sc.nextInt();
+
+    }
+}
