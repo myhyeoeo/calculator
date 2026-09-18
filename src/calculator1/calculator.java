46a4815e4d7ff994c8c7c3e2e7f1f5c7adbbe47d
myhyeoeo <dk9790@naver.com>
2026-09-18 09:42:07 +0900

    feat:사칙연산 기호(➕,➖,✖️,➗)를 입력받기

--- src/calculator1/calculator.java
+++ src/calculator1/calculator.java
@@ -7,10 +7,15 @@ public class calculator {
         Scanner sc = new Scanner(System.in);
         int a = 0;
         int b = 0;
+        char op = ' ';
+
         System.out.print("첫 번째 숫자를 입력하세요: ");
         a = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         b = sc.nextInt();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        op = sc.nextLine().charAt(0);
 
     }
 }
