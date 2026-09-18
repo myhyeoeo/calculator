65eb3bbe9c115a0c9533810f6a824905c228952d
myhyeoeo <dk9790@naver.com>
2026-09-18 11:26:24 +0900

    feat:exit 문자열을 입력하기 전까지 무한으로 계산

--- src/calculator1/calculator.java
+++ src/calculator1/calculator.java
@@ -10,36 +10,45 @@ public class calculator {
         int b = 0;
         char op = ' ';
         int result = 0;
+        String repeat = "";
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        a = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        b = sc.nextInt();
-        sc.nextLine();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        op = sc.nextLine().charAt(0);
+        while(!repeat.equals("exit")){
 
-        if(a<0 || b<0){
-            System.out.println("첫 번째 숫자와 두 번째 숫자는 양의 정수여야 합니다");
-        }
-        else if(op!='➕' && op!='➖' && op!='✖' && op!='➗'){
-            System.out.println("사칙연산 기호는 ➕,➖,✖,➗ 중에 입력해야 합니다");
-        }
-        else{
-            if(op=='➕'){
-                result = a+b;
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            a = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            b = sc.nextInt();
+            sc.nextLine();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            op = sc.nextLine().charAt(0);
+
+            if(a<0 || b<0){
+                System.out.println("첫 번째 숫자와 두 번째 숫자는 양의 정수여야 합니다");
             }
-            else if (op=='➖') {
-                result = a-b;
+            else if(op!='➕' && op!='➖' && op!='✖' && op!='➗'){
+                System.out.println("사칙연산 기호는 ➕,➖,✖,➗ 중에 입력해야 합니다");
             }
-            else if (op=='✖'){
-                result = a*b;
+            else{
+                if(op=='➕'){
+                    result = a+b;
+                }
+                else if (op=='➖') {
+                    result = a-b;
+                }
+                else if (op=='✖'){
+                    result = a*b;
+                }
+                else if(op=='➗'){
+                    if(b==0) System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
+                    else result = a/b;
+                }
+                System.out.println("결과: "+result);
             }
-            else if(op=='➗'){
-                if(b==0) System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
-                else result = a/b;
+            System.out.println("더 계산하시겠습니까?(exit 입력 시 종료): ");
+            repeat = sc.nextLine();
+            if(repeat.equals("exit")){
+                System.out.println("프로그램을 종료합니다");
             }
-            System.out.println("결과: "+result);
         }
 
 
